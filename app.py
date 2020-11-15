cfcb7144533b788a72b2e70f75336755d304fd5d
Pratik Yadav <pratikyadav33@gmail.com>
2020-11-15 15:51:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,85 +1,97 @@
+!pip install gevent
 
-from PIL import Image, ImageOps
-
+import os 
+import pickle
+import tensorflow as tf
+    
+import requests
 from keras.preprocessing.sequence import pad_sequences
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.image import load_img, img_to_array
-from keras.models import Model
 from keras.models import load_model
 from keras.applications.inception_v3 import InceptionV3, preprocess_input
+from keras.models import Model
+from keras.preprocessing.image import load_img, img_to_array
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+from flask import Flask, redirect, url_for, request, render_template
 
-import pickle
-import string
-import streamlit as st
-import tensorflow as tf
 
-st.title("Image Caption")
-st.text("Tell Caption Tell Caption Tell Caption Tell Caption")
+app = Flask(__name__)
 
-new_model=tf.keras.models.load_model("./my_model.h5")
+#Loadning Necessary Files And Documents
 
-def load_model():
-  
-  w2i_file=open("./wordtoix.p","rb")
-  wordtoix = pickle.load(w2i_file)
+new_model= load_model("/content/gdrive/My Drive/Colab Notebooks/my_model.h5")
 
-  i2w_file=open("./ixtoword.p","rb")
-  ixtoword = pickle.load(i2w_file)
+w2i_file = open("/content/gdrive/My Drive/Colab Notebooks/wordtoix.p","rb")
+wordtoix = pickle.load(w2i_file)
 
-  base_model = InceptionV3(weights = 'imagenet')
+i2w_file=open("/content/gdrive/My Drive/Colab Notebooks/ixtoword.p","rb")
+ixtoword = pickle.load(i2w_file)
 
-  model = Model(base_model.input, base_model.layers[-2].output)
+base_model = InceptionV3(weights = 'imagenet')
 
-with st.spinner("Loading Model Into Memory"):
-  load_model()
+model = Model(base_model.input, base_model.layers[-2].output)
 
 max_length = 34
 
-def preprocess_img(img):
-  #inception v3 excepts img in 299*299
+print('Model loaded')
 
-  #img = load_img(requests.get(img_path).content, target_size = (299, 299))
-  x = img_to_array(img)
-  # Add one more dimension
-  x = np.expand_dims(x, axis = 0)
-  x = preprocess_input(x)
-  return x
+def preprocess_img(img_path):
+    #inception v3 excepts img in 299*299
+    img = load_img(img_path, target_size = (299, 299))
+    x = img_to_array(img)
+    # Add one more dimension
+    x = np.expand_dims(x, axis = 0)
+    x = preprocess_input(x)
+    return x
 
 
-def encode(im):
-  image = preprocess_img(im)
-  vec = model.predict(image)
-  vec = np.reshape(vec, (vec.shape[1]))
-  return vec
+def encode(image):
+    image = preprocess_img(image)
+    vec = model.predict(image)
+    vec = np.reshape(vec, (vec.shape[1]))
+    return vec
     
 def greedy_search(pic):
-  start = 'startseq'
-  for i in range(max_length):
-      seq = [wordtoix[word] for word in start.split() if word in wordtoix]
-      seq = pad_sequences([seq], maxlen = max_length)
-      yhat = new_model.predict([pic, seq])
-      yhat = np.argmax(yhat)
-      word = ixtoword[yhat]
-      start += ' ' + word
-      if word == 'endseq':
-          break
-  final = start.split()
-  final = final[1:-1]
-  final = ' '.join(final)
-  return final
+    start = 'startseq'
+    for i in range(max_length):
+        seq = [wordtoix[word] for word in start.split() if word in wordtoix]
+        seq = pad_sequences([seq], maxlen = max_length)
+        yhat = new_model.predict([pic, seq])
+        yhat = np.argmax(yhat)
+        word = ixtoword[yhat]
+        start += ' ' + word
+        if word == 'endseq':
+            break
+    final = start.split()
+    final = final[1:-1]
+    final = ' '.join(final)
+    return final
 
-file = st.file_uploader("Upload Photo",type=["jpg","png"])
+app = Flask(__name__)
 
-if file is None:
-  st.text("upoad image")
-else:
-  image = Image.open(file)
-  st.image(image , use_colum_width = True)
-  image = ImageOps.fit(image,(299,299),Image.ANTIALIAS)
-  pic = encode(image)
-  img = pic.reshape(1, 2048)
-  final_caption = (greedy_search(img))
-  st.write(final_caption)
-  
+@app.route('/',methods=["GET"])
+def index():
+    return render_template('index.html')
+
+@app.route('/predict',methods=["GET",'POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        basepath = '/content/gdrive/My Drive/Colab Notebooks/'
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+        pic = encode(file_path)
+        img = pic.reshape(1, 2048)
+        caption = greedy_search(img)
+
+        pic = encode(file_path)
+        img = pic.reshape(1, 2048)
+        caption = greedy_search(img)
+        
+        os.remove(file_path)
+        return caption
+    return None
+
 if __name__ == "__main__":
-  main()
+    app.run()
